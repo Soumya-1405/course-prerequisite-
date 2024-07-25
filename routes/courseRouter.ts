439590850e7e59afe3bac3df57b1a4815fc3ed57
katta soumya <kattasoumya2002@gmail.com>
2024-07-25 10:13:28 +0530

    create some routes

---
+++ routes/courseRouter.ts
@@ -0,0 +1,53 @@
+import express from 'express'
+import { CourseModel } from '../collections/coursePrerequisite';
+
+export const courseRouter = express.Router();
+
+courseRouter.get('/allCourses',async(req,res)=>{
+    try{
+        const courses = await CourseModel.find();
+        res.send(courses)
+    }
+    catch(e){
+        console.log("Error:",e);
+    }
+})
+
+courseRouter.get('/:id',async(req,res)=>{
+    const id = req.params.id;
+    try{
+        const course = await CourseModel.findById({_id:id})
+        res.send(course);
+    }
+    catch(e){
+        res.send("error")
+        console.log("Error:",e);
+    }
+
+})
+
+courseRouter.put('/:id',async(req,res)=>{
+    const id = req.params.id;
+    try{
+        const course = await CourseModel.findByIdAndUpdate({_id:id},req.body)
+        res.send(course);
+    }
+    catch(e){
+        res.send("error")
+        console.log("Error:",e);
+    }
+
+})
+
+courseRouter.delete('/:id',async(req,res)=>{
+    const id = req.params.id;
+    try{
+        const course = await CourseModel.findByIdAndDelete({_id:id})
+        res.send(course);
+    }
+    catch(e){
+        res.send("error")
+        console.log("Error:",e);
+    }
+
+})
